2b5b51bc96fe3caaa4e739ec9c486dac30dc553f
You Bun Two <ppartyka@ubuntu.(none)>
2013-10-30 20:27:06 -0400

    updated menu build

--- menu.py
+++ menu.py
@@ -2,49 +2,49 @@
 
 
 MENU = {"Tickets":["Open Ticket", "Close Ticket", "Modify Ticket"], "Search":["Open Tickets", "Closed Tickets", "Ticket Numbers"]}
+EXQT = ["exit", "quit", "close", "Exit", "Quit", "Close"]
+BACK = ["back", "b", "B", "Back", "previous", "p", "P", "Previous"]
 
 def menu_main(MNU):
-	print("1.{0}\n2.{1}\n".format(*MNU.keys()))
-	sel = raw_input("Selection:")
-	if str(sel) not in "12":
-		print "Select a Number between 1 and 2 please."
-	elif not sel:
+	print("1.{0}   2.{1}".format(*MNU.keys()))
+	sel = raw_input("> ")
+	if not sel or sel in EXQT:
 		exit()
+	elif sel not in "12":
+		print("1 or 2 please.")	
 	else:
 		return str(sel)
 
 def selection():
-	sel = raw_input("> ")
-	if sel not in "123":
-		print("Select a Number between 1 and 3 please.")
-	elif not sel:
-		exit()
+	count = 0
+	while count < 3:
+		sel = raw_input("> ")
+		if sel in BACK:
+			break
+		elif sel in EXQT:
+			exit()
+		elif sel not in "123":
+			count += 1	
+		else:
+			return str(sel)
+
+def menu_opt(men_return):
+	print("")
+	if men_return == "1":
+		print("Tickets Menu:")
+		print("1.{0}   2.{1}   3.{2}".format(*MENU["Tickets"]))
+		selection()
+	elif men_return == "2":
+		print("Search Menu:")
+		print("1.{0}   2.{1}   3.{2}".format(*MENU["Search"]))
+		selection()
 	else:
-		return str(sel)
+		print("Error 42")
 
-def menu_tickets(MNU):
-	print("")
-	print("Tickets Menu:")
-	print("1.{0}   2.{1}   3.{2}".format(*MNU["Tickets"]))
-	print("")
-	selection()
-
-def menu_search(MNU):
-	print("")
-	print("Search Menu:")
-	print("1.{0}   2.{1}   3.{2}".format(*MNU["Search"]))
-	print("")
-	selection()
-
-
-while True:
-	print("")
-	print("Welcome to Tikpy, a simple python ticketing system; please select from the following menu")
-	print ""
-	select = menu_main(MENU)
-	if select == "1":
-		menu_tickets(MENU)
-	elif select == "2":
-		menu_search(MENU)
-	else:
-		print("Error Wrong Input")
+if __name__ == "__main__":
+		while True:
+			print("")
+			print("Welcome to Tikpy, a simple python ticketing system; please select from the following menu:")
+			print("")
+			MENU1 = menu_main(MENU)
+			menu_opt(MENU1)
