64d95a6d2aa9a22936c453b20963b2cdbd4a0528
ejones <ejones85@gmail.com>
2013-11-06 19:21:06 -0500

    Updated version of Tikpy with write, update, and 2 search queries

--- Tikpy.py
+++ Tikpy.py
@@ -7,11 +7,8 @@ def dbconnect(host, user, password, database):
 	return con
 
 class Tikpy(object):
-	def __init__(self, status, description, solution, uid="uid"):
-		self.status = status
-		self.description = description
-		self.solution = solution
-		self.uid = uid
+	def __init__(self):
+		pass
 
 	def write(self):
 		status = "OPN"
@@ -27,10 +24,56 @@ class Tikpy(object):
 			con.close()
 		
 
-	def update(self, status, solution):
-		pass
-	def select(self):
-		pass
+	def update(self, modtype):
+		con = dbconnect('localhost', 'root', 'root', 'tikpy')
+		if modtype == 'close':
+			solution = raw_input("How did you solve the issue?: ")
+			ID = raw_input("What is the ticket number: ")
+			with con:
+				cur = con.cursor()
+				cur.execute("UPDATE Ticket SET Status='CLD', Solution='%s' WHERE ID='%s'" 
+				% (solution, ID))
 
-test = Tikpy("OPN", "Test", "Test")
-test.write()
+			if con:
+				con.close()
+
+		elif modtype == 'modify':
+			description = raw_input("What would you like to add?: ")
+			ID = raw_input("What is the ticket number: ")
+			with con:
+				cur = con.cursor()
+				cur.execute("SELECT Description FROM Ticket WHERE ID ='%s'" 
+				% (ID))
+				old_desc = cur.fetchone()
+				cur.execute("UPDATE Ticket SET Description='%s Addition():%s' WHERE ID='%s'" 
+				% (old_desc[0], description, ID))
+
+			if con:
+				con.close()
+
+	def select(self, search_type):
+		con = dbconnect('localhost', 'root', 'root', 'tikpy')
+		if search_type == 'open':
+			with con:
+				cur = con.cursor(MySQLdb.cursors.DictCursor)
+				cur.execute("SELECT * FROM Ticket WHERE Status = 'OPN'")
+				result = cur.fetchall()
+				for x in result:
+					print x
+
+			if con:
+				con.close()
+
+		if search_type == 'closed':
+			with con:
+				cur = con.cursor(MySQLdb.cursors.DictCursor)
+				cur.execute("SELECT * FROM Ticket WHERE Status = 'CLD'")
+				result = cur.fetchall()
+				for x in result:
+					print x
+
+			if con:
+				con.close()
+
+test = Tikpy()
+test.select('closed')
