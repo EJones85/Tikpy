b4c0b3fc21d9eec96070ec3acaa2a907abb13d41
ejones <ejones85@gmail.com>
2013-11-17 15:14:27 -0500

    Corrected error with ticket number search

--- Tikpy.py
+++ Tikpy.py
@@ -91,7 +91,7 @@ class Tikpy(object):
 			search_value = raw_input("What is the ticket number: ")
 			with con:
 				cur = con.cursor(MySQLdb.cursors.DictCursor)
-				cur.execute("SELECT * FROM ticket WHERE ID = %s".format(search_value))
+				cur.execute("SELECT * FROM ticket WHERE ID = {0}".format(search_value))
 				result = cur.fetchall()
 				print("ID    Status    UID   Description")
 				for x in result:
