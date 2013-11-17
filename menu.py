d7f7b8e0dcf2746948ea2345ba18a4453f54b48d
You Bun Two <ppartyka@ubuntu.(none)>
2013-11-17 12:05:52 -0500

    commented menu function

--- menu.py
+++ menu.py
@@ -5,10 +5,16 @@ import Tikpy
 Tik = Tikpy.Tikpy()
 
 MENU = {"Tickets":["Open Ticket", "Close Ticket", "Modify Ticket"], "Search":["Open Tickets", "Closed Tickets", "Ticket Numbers"]}
-EXQT = ["exit", "quit", "close", "Exit", "Quit", "Close"]
+EXQT = ["exit", "quit", "close", "Exit", "Quit", "Close", "EXIT", "QUIT", "CLOSE", "e", "q", "c", "E", "Q", "C"]
 BACK = ["back", "b", "B", "Back", "previous", "p", "P", "Previous"]
+TIK_MEN = [x for x in MENU["Tickets"]]
+SCH_MEN = [x for x in MENU["Search"]]
 
 def menu_main(MNU):
+	""" STDIN > STR
+	Menu function for top level menu:
+	valid selecitons 1, 2 will exit if no input is given or input from EXQT list is given
+	"""
 	print("1.{0}   2.{1}".format(*MNU.keys()))
 	sel = raw_input("> ")
 	if not sel or sel in EXQT:
@@ -18,7 +24,12 @@ def menu_main(MNU):
 	else:
 		return str(sel)
 
-def selection():
+def selection(sel_list):
+	""" STDIN > STR
+	Selection function for sub menus tickets and search checks input between 1 and menu list
+	length. Will exit if input is in EXQT, or if input is wrong 3 times will go back to
+	main menu
+	"""
 	count = 0
 	while count < 3:
 		sel = raw_input("> ")
@@ -26,17 +37,21 @@ def selection():
 			break
 		elif sel in EXQT:
 			exit()
-		elif sel not in "123":
+		elif sel not in [str(x) for x in range(1,len(sel_list) + 1)]:
 			count += 1	
 		else:
 			return str(sel)
 
 def menu_opt(men_return):
+	""" STDIN > None
+	This is a menu option function that performs a call on the Tikpy module to edit the 
+	backend database based on the user selection. Takes input from main_menu function.
+	"""
 	print("")
 	if men_return == "1":
 		print("Tickets Menu:")
 		print("1.{0}   2.{1}   3.{2}".format(*MENU["Tickets"]))
-		var = selection()
+		var = selection(TIK_MEN)
 		if var == "1":
 			Tik.write()
 		elif var == "2":
@@ -44,11 +59,11 @@ def menu_opt(men_return):
 		elif var == "3":
 			Tik.update('modify')
 		else:
-			print "Error 43"
+			print("Error in Ticket menu function in menu_opt function.")
 	elif men_return == "2":
 		print("Search Menu:")
 		print("1.{0}   2.{1}   3.{2}".format(*MENU["Search"]))
-		var = selection()
+		var = selection(SCH_MEN)
 		if var == "1":
 			Tik.select("open")
 		elif var == "2":
@@ -56,9 +71,9 @@ def menu_opt(men_return):
 		elif var == "3":
 			Tik.select("ticket")
 		else:
-			print "Error 44"
+			print("Error 44")
 	else:
-		print("Error 42")
+		print("Error in Search menu function in menu_opt function.")
 
 if __name__ == "__main__":
 		while True:
